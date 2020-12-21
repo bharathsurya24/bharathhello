f9a9898a1757d46f14d8788c09eeb487eb2ce453
YA20169380 <YA20169380@APL-F0SB403>
2020-12-21 14:22:15 +0530

    ok

---
+++ bharathello/src/main/java/com/example/BharathelloApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BharathelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BharathelloApplication.class, args);
+		System.out.println("hellO");
+	}
+
+}
